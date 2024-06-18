414bad45ad779e07cf2df34f51770914d2deabad
Sarah Tambalo <sarahbianca90@gmail.com>
2024-06-18 18:50:10 -0300

    adição dos diretorios de teste de terminal e desafio ContaBanco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        //TODO:Conhecer e importar a classe Scanner
 
-        //Exibir as mensagens para o nosso usuário
+        Scanner sc = new Scanner(System.in);
 
-        //Obter pela classe Scanner os valores digitados no terminal
+        System.out.print("Por favor, digite o número de sua agência: ");
+        String Agencia = sc.nextLine();
+        System.out.println();
 
-        //Exibir a mensagem da conta criada
+        System.out.print("Agora digite o número de sua conta: ");
+        String numeroContaStr = sc.nextLine();
+        int Numero = Integer.parseInt(numeroContaStr);
+        System.out.println();
+
+        System.out.print("Informe seu nome completo: ");
+        String NomeCliente = sc.nextLine();
+        System.out.println();
+
+        System.out.print("Informe o valor do depósito inicial: ");
+        String saldoStr = sc.nextLine();
+        double Saldo = Double.parseDouble(saldoStr);
+
+        sc.close();
+
+        DadosCliente cliente = new DadosCliente(Numero, Agencia, NomeCliente, Saldo);
+
+        System.out.println(cliente.toString());
     }
 }
